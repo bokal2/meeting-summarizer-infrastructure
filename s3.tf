76ab511de1078fd789f4108bb849fc87e4e648f0
Erick Okal <erickokal@Okinja.local>
2025-01-22 18:11:39 +0300

    refactor(refactor): Updated s3 resource

--- s3.tf
+++ s3.tf
@@ -2,13 +2,10 @@
 resource "random_id" "this" {
   byte_length = 4
 }
-
-
 resource "aws_s3_bucket" "this" {
   bucket = "meeting-recordings-${random_id.this.hex}"
 
   tags = {
-    ManagedBy = "Terraform"
-    Project   = var.project_name
+    project_name = var.project_name
   }
 }
